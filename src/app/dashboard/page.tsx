da2ca67534cf2d60cdc996cc5faa023124062753
zahidul-islam-144 <z.islam.nishat@gmail.com>
2024-03-09 04:13:41 +0600

    installed shadcn

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,12 @@
 import React, { FC } from 'react';
+import { Button } from "@/components/ui/button"
+
 
 const Dashboard: FC = () => {
     return (
         <section className='dashboard_main'>
           <p className='text'>This is dashboard page</p>
+
         </section>
     );
 };
